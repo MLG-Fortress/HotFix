f939e1af3dd5efcc15db62f2dabbeaa244380f44
RoboMWM <robomwm@users.noreply.github.com>
2016-02-13 00:11:56 -0800

    debug

--- src/main/java/me/robomwm/HotFix/Main.java
+++ src/main/java/me/robomwm/HotFix/Main.java
@@ -1,5 +1,9 @@
 package me.robomwm.HotFix;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,6 +19,21 @@ public class Main extends JavaPlugin implements Listener
     public void onEnable()
     {
         getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    {
+        if (cmd.getName().equalsIgnoreCase("hotfix"))
+        {
+            if (args.length < 2)
+                return false;
+
+            Player player = Bukkit.getPlayer(args[0]);
+            player.setPlayerListName(args[1]);
+            return true;
+        }
+        return false;
     }
 
     // ProjectKorra hotfix: This code cancels Explosions from other plugins
