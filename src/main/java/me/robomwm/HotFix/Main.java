db75f0108487c5ab0058a726cf3f2b3aedafa1af
RoboMWM <robomwm@users.noreply.github.com>
2016-04-07 00:14:12 -0700

    every time enderman teleport, this should print yes?

--- src/main/java/me/robomwm/HotFix/Main.java
+++ src/main/java/me/robomwm/HotFix/Main.java
@@ -1,14 +1,18 @@
 package me.robomwm.HotFix;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -38,18 +42,27 @@ public class Main extends JavaPlugin implements Listener
 
     // ProjectKorra hotfix: This code cancels Explosions from other plugins
     // https://github.com/ProjectKorra/ProjectKorra/issues/400
-    @EventHandler (priority = EventPriority.LOWEST)
-    public void onEntityDamageByBlock(EntityDamageByBlockEvent event)
-    {
-        if (!event.isCancelled())
-            return;
+//    @EventHandler (priority = EventPriority.LOWEST)
+//    public void onEntityDamageByBlock(EntityDamageByBlockEvent event)
+//    {
+//        if (!event.isCancelled())
+//            return;
+//
+//        if (event.getCause().equals(DamageCause.BLOCK_EXPLOSION))
+//        {
+//            if (event.getDamager() == null)
+//            {
+//            	event.setCancelled(false);
+//            }
+//        }
+//    }
 
-        if (event.getCause().equals(DamageCause.BLOCK_EXPLOSION))
-        {
-            if (event.getDamager() == null)
-            {
-            	event.setCancelled(false);
-            }
-        }
+    @EventHandler
+    public void onEntityTeleport(EntityTeleportEvent event)
+    {
+        Bukkit.broadcastMessage("loud noises");
+        Entity entity = event.getEntity();
+        World world = entity.getWorld();
+        Bukkit.broadcastMessage(entity.getType().toString() + world.getName());
     }
 }
