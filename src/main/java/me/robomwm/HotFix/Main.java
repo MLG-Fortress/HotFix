d9f5aba35ef263562e3a72e6abb50fbca88614f2
RoboMWM <robomwm@outlook.com>
2016-08-11 13:37:47 -0700

    try to investigate y dis iz

--- src/main/java/me/robomwm/HotFix/Main.java
+++ src/main/java/me/robomwm/HotFix/Main.java
@@ -46,6 +46,7 @@ public class Main extends JavaPlugin implements Listener
             }
             FallingBlock entity = (FallingBlock)event.getEntity();
             Block block = event.getBlock();
+            Bukkit.broadcastMessage(block.getLocation().toString());
             event.setCancelled(true);
             @SuppressWarnings("deprecation")
             ItemStack itemStack = new ItemStack(entity.getMaterial(), 1, entity.getBlockData());
