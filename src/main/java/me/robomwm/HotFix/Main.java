ec9fc0282744e10b340282970d0775724705059b
RoboMWM <robomwm@users.noreply.github.com>
2018-09-20 15:41:30 -0700

    place, also 1.13

--- src/main/java/me/robomwm/HotFix/Main.java
+++ src/main/java/me/robomwm/HotFix/Main.java
@@ -22,7 +22,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -376,7 +376,7 @@ public class Main extends JavaPlugin implements Listener {
 //    }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    private void onBlockBreak(BlockBreakEvent event)
+    private void onBlockBreak(BlockPlaceEvent event)
     {
         if (!herp)
             return;
